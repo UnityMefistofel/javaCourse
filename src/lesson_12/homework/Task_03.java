73fcc6a5f7c41a321977671e8d87614d05319b40
UnityMefistofel <mefistofel.sf@mail.ru>
2022-12-06 01:01:13 +0300

    MainStream

--- src/lesson_12/homework/Task_03.java
+++ src/lesson_12/homework/Task_03.java
@@ -3,6 +3,7 @@ package lesson_12.homework;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.HashMap;
 
 public class Task_03 {
@@ -11,26 +12,44 @@ public class Task_03 {
 //      Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
 //      Новая задача: Программа должна работать не с номерами домов, а с городами:
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         HashMap<String, String> map = new HashMap<>();
+
+        System.out.println("- Заполнение справочника -");
         while (true) {
-            String city = reader.readLine();
+            System.out.print("Введите название города: ");
+            String city = getString(reader);
             if (city.isEmpty()) {
                 break;
             }
-            String family = reader.readLine();
+
+            System.out.print("Введите фамилию жителя: ");
+            String family = getString(reader);
 
             map.put(city, family);
         }
 
-        String city = reader.readLine();
+        System.out.println("\n-Поиск значения по ключу -");
+        System.out.print("Введите ключ: ");
+        String city = getString(reader);
 
         if (map.containsKey(city)) {
             String familyName = map.get(city);
-            System.out.println(familyName);
+            System.out.println("Значение: " + familyName);
         }
+    }
+
+    public static String getString(BufferedReader reader) {
+
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 
 
